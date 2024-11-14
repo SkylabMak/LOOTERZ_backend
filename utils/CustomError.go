f305daca4af72754e5e7418bc18ededa1c64595c
praisan <skylab135@gmail.com>
2024-11-14 09:10:31 +0700

    add Custom Error

--- utils/CustomError.go
+++ utils/CustomError.go
@@ -5,16 +5,25 @@ import (
 )
 
 // ErrorResponse sends a standardized error response
-func ErrorResponse(c *fiber.Ctx,statusCode int, message string,  detail string) error {
+func ErrorResponse(c *fiber.Ctx, statusCode int, errorCode string, message string, detail string) error {
 	return c.Status(statusCode).JSON(fiber.Map{
+		"code":    errorCode,
 		"error":   message,
 		"details": detail,
 	})
 }
 
-func FullErrorResponse(c *fiber.Ctx, statusCode int, message string, err error) error {
+func FullErrorResponse(c *fiber.Ctx, statusCode int, errorCode string, message string, err error) error {
 	return c.Status(statusCode).JSON(fiber.Map{
+		"code":    errorCode,
 		"error":   message,
 		"details": err.Error(),
+	})
+}
+
+func CustomErrorResponse(c *fiber.Ctx, statusCode int, errorCode string, header string, message string) error {
+	return c.Status(statusCode).JSON(fiber.Map{
+		"code": errorCode,
+		header: message,
 	})
 }
