14de4bc5cb19114caefd8fdcf07be4920da05126
praisan <skylab135@gmail.com>
2024-11-14 07:59:18 +0700

    add column DB

--- models/modelsDB/user.go
+++ models/modelsDB/user.go
@@ -5,6 +5,8 @@ type User struct {
 	UserName string `gorm:"column:UserName"`
 	Head     bool   `gorm:"column:head"`
 	RoomID   string `gorm:"column:roomID"`
+	ImgID    string `gorm:"column:imgID"`
+	Ready    bool   `gorm:"column:ready"`
 
 	Room *Room `gorm:"foreignKey:RoomID"`
 }
